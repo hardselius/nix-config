c592541a34133a14d9d61d156130bb265bb02e69
Martin Hardselius <martin@hardselius.dev>
2025-08-20 14:40:38 +0200

    Remove tfenv from PATH

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -75,7 +75,7 @@ in
       export PATH=$HOME/.pnpm-packages/bin:$HOME/.pnpm-packages:$PATH
       export PATH=$HOME/.npm-packages/bin:$HOME/bin:$PATH
       export PATH=$HOME/.local/share/bin:$PATH
-      export PATH=$HOME/bin:$HOME/.local/bin:$HOME/.tfenv/bin:$HOME/.cargo/bin:$PATH
+      export PATH=$HOME/bin:$HOME/.local/bin:$HOME/.cargo/bin:$PATH
 
       # Remove history data we don't want to see
       export HISTIGNORE="pwd:ls:cd"
