37dc0f1bd229ca946876559daef805f8a389568e
Martin Hardselius <martin@hardselius.dev>
2026-09-23 10:01:34 +0200

    Fix deprecation warning

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -222,7 +222,7 @@ in
 
   ghostty = {
     enable = true;
-    package = if stdenv.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
+    package = if stdenv.hostPlatform.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
     settings = {
       theme = "light:Alabaster, dark:Alabaster Dark";
       font-family = "Aporetic Sans Mono";
@@ -419,7 +419,7 @@ in
     scdaemonSettings = {
       disable-ccid = true;
     }
-    // lib.optionalAttrs pkgs.stdenv.isDarwin {
+    // lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
       reader-port = ''"Yubico YubiKey OTP+FIDO+CCID"'';
     };
   };
